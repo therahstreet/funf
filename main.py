4c63eb40178ceba49a507b84648ea86fc83118fb
therahstreet <109562558+therahstreet@users.noreply.github.com>
2022-07-19 03:41:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from sys import platform as oc
+import sqlite3 as sql
+import time
+from random import randint
+
+if oc == 'win32':
+	os.system("cls")
+	print("FUNF")
+
+
+elif oc == 'linux' or oc == 'linux2':
+	os.system("clear")
+	print("FUNF")
+
+
+
+
+
+
